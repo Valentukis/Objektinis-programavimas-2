d1386ff34180bb653447cad7e6a1f8423f0a676a
Valentinas <181562313+Valentukis@users.noreply.github.com>
2025-02-06 20:55:45 +0200

    Prideti medianos skaiciavima

--- mano_lib.h
+++ mano_lib.h
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <cmath>
+
 using std::string;
 using std::cout;
 using std::endl;
 using std::vector;
 using std::endl;
 using std::cin;
+using std::sort;
 
 struct Stud{
     string var, pav;
     vector<int> paz;
     int egz;
     double vidurkis;
+    double mediana;
 };
--- obj_1_prat.cpp
+++ obj_1_prat.cpp
@@ -3,26 +3,37 @@
 int main(){
 
     vector <Stud> grupe;
-    int sum, atsisk_paz;
-
+    int sum, atsisk_paz, medianos_poz;
     for (int i = 0; i < 2; i++) {
         Stud laik;
         sum = 0;
         cin >> laik.var;
         cin >> laik.pav;
         
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 4; i++) {
             cin >> atsisk_paz;
             sum += atsisk_paz;
             laik.paz.push_back(atsisk_paz);
         }
         cin >> laik.egz;
         laik.vidurkis = double(sum) / laik.paz.size();
+
+        sort(laik.paz.begin(), laik.paz.end());
+
+        if (laik.paz.size() % 2 == 0) {
+            medianos_poz = laik.paz.size() / 2;
+            laik.mediana = ( laik.paz.at(medianos_poz) + laik.paz.at(medianos_poz - 1) ) / 2.0;
+        }
+        else {
+            medianos_poz = floor(laik.paz.size() / 2);
+            laik.mediana = laik.paz.at(medianos_poz);
+        }
+        
         grupe.push_back(laik);
     }
 
     for (auto n: grupe) {
-        cout << endl << n.var << " " << n.pav << " " << n.egz << " " << n.vidurkis << endl;
+        cout << endl << n.var << " " << n.pav << " " << n.egz << " " << n.vidurkis << " " << n.mediana << endl;
         for (auto m: n.paz) cout << m << " ";
     }
 
