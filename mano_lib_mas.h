598bb533993fc26275705a1fd17c9beb68947c73
Valentinas <181562313+Valentukis@users.noreply.github.com>
2025-02-09 17:02:55 +0200

    Sukurti naujus failus 2 atskiroms versijoms

---
+++ mano_lib_mas.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <vector>
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::endl;
+using std::cin;
+using std::sort;
+using std::setw;
+using std::vector;
+struct Stud{
+    string var, pav;
+    int paz[100];
+    int egz;
+    double vidurkis;
+    double mediana;
+    double galutinis;
+};
