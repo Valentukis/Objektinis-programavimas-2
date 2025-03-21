b8f243f4a1e8a3164bbf876d3258aed9cf1f18a5
Valentinas <181562313+Valentukis@users.noreply.github.com>
2025-03-21 20:49:49 +0200

    Paruosti atskira aplanka klases realizacijai

---
+++ class/mano_lib.h
@@ -0,0 +1,38 @@
+#ifndef MANO_BIBLIOTEKA
+#define MANO_BIBLIOTEKA
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <random>
+#include <ctime>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <chrono>
+#include <limits>
+#include <exception>
+
+using std::istringstream;
+using std::string;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::endl;
+using std::cin;
+using std::sort;
+using std::setw;
+using std::ifstream;
+using std::ofstream;
+
+struct Stud{
+    string var, pav;
+    vector<int> paz;
+    int egz;
+    double vidurkis;
+    double mediana;
+    double galutinis_pagal_vid;
+    double galutinis_pagal_med;
+};
+#endif
---
+++ class/studentas.h
@@ -0,0 +1,12 @@
+#include "mano_lib.h"
+
+class Studentas {
+    private:
+        string var_, pav_;
+        vector<int> paz;
+        int egz_;
+        double vidurkis;
+        double mediana;
+        double galutinis_pagal_vid;
+        double galutinis_pagal_med;
+        }
