4e0db633e012a0102d51dd3dae294186af430d14
Valentinas <181562313+Valentukis@users.noreply.github.com>
2025-03-22 15:13:58 +0200

    Prideti ivedimo metodus

--- class/studentas.cpp
+++ class/studentas.cpp
@@ -18,7 +18,7 @@ Studentas::Studentas(std::istream& is, int sk) {
 }
 
 void Studentas::sugeneruoti_lyti() {
-    int lytis_ = rand() % 2;  //0 - moteris, 1 - vyras;
+    lytis_ = rand() % 2;  //0 - moteris, 1 - vyras;
 }
 
 void Studentas::sugeneruoti_varda() {
@@ -38,5 +38,127 @@ void Studentas::sugeneruoti_pavarde() {
     else pav_ = moteru_pavardes[pasirinkimas];
 }
 
+void Studentas::ivedimas_ranka() {
+    int i = 0, sum = 0;
+    int atsisk_paz;
 
+    while (true) {
 
+        try {
+        cout << "Įveskite studento vardą: ";
+        cin >> var_;
+        if (cin.fail()) throw std::runtime_error("Netinkamas vardo įvedimas. Bandykite vėl.");
+
+        cout << "Įveskite studento pavardę: ";
+        cin >> pav_;
+        if (cin.fail()) throw std::runtime_error("Netinkamas pavardės įvedimas. Bandykite vėl.");
+
+        while(true){
+            cout << "Įveskite " << i + 1 << "-ą atsiskaitymo pažymį (Arba, jei baigėte pažymių įvedimą, įveskite -1): ";
+            i++;
+            cin >> atsisk_paz;
+            if (cin.fail() || (atsisk_paz < 0 && atsisk_paz != -1)) throw std::runtime_error("Netinkamas pažymio įvedimas. Bandykite vėl.");
+
+            if (atsisk_paz == -1) break;
+            sum += atsisk_paz;
+            paz_.push_back(atsisk_paz);
+        }
+
+        cout << "Įveskite egzamino įvertinimą: ";
+        cin >> egz_;
+        if (cin.fail() || egz_ < 0 || egz_ > 10) throw std::runtime_error("Netinkamas egzamino įvedimas. Bandykite vėl.");
+        break;
+        }
+
+        catch(const std::exception &e) {
+            std::cerr << e.what() << endl;
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            paz_.clear();
+            sum = 0;
+            i = 0;
+        }
+
+    }
+
+    paskaiciuoti_vid_ir_med();
+    paskaiciuoti_gal();
+}
+
+void Studentas::ivedimas_pazymiu_generavimu() {
+    int paz_sk, pazymys, sum = 0;
+
+    while(true) {
+        try {
+        cout << "Įveskite studento vardą: ";
+        cin >> var_;
+        if (cin.fail()) throw std::runtime_error("Netinkamas vardo įvedimas. Bandykite vėl.");
+        cout << "Įveskite studento pavardę: ";
+        cin >> pav_;
+        if (cin.fail()) throw std::runtime_error("Netinkamas pavardės įvedimas. Bandykite vėl.");
+        
+        cout << "Kiek pažymiu sugeneruoti šiam studentui? ";
+        cin >> paz_sk;
+        if (cin.fail() || paz_sk < 0 || paz_sk > 10) throw std::runtime_error("Netinkamas pažymių skaičiaus įvedimas. Bandykite vėl.");
+
+        for (int i = 0; i < paz_sk; i++) {
+            pazymys = rand() % 10 + 1;
+            paz_.push_back(pazymys);
+            sum += pazymys;
+            cout << i + 1 << " pažymys: " << paz_.at(i) << endl;
+        }
+
+        egz_ = rand() % 10 + 1;
+        cout << "Egzamino įvertinimas: " << egz_ << endl;
+        break;
+    }
+    catch(const std::exception &e) {
+        std::cerr << e.what() << endl;
+        cin.clear();
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        paz_.clear();
+        sum = 0;
+    }
+    }
+
+    paskaiciuoti_vid_ir_med();
+    paskaiciuoti_gal();
+}
+
+void Studentas::ivedimas_generuojant_viska() {
+    sugeneruoti_lyti();
+    sugeneruoti_varda();
+    sugeneruoti_pavarde();
+
+    int paz_sk, pazymys, sum = 0;
+
+    cout << "Vardas - " << var_ << endl;
+    cout << "Pavardė - " << pav_ << endl;
+    
+    while(true) {
+        try{
+            cout << "Kiek pažymiu sugeneruoti šiam studentui? ";
+            cin >> paz_sk;
+            if (cin.fail() || paz_sk < 0) throw std::runtime_error("Netinkamas pažymių skaičiaus įvedimas. Bandykite įvesti jį vėl.");
+            break;
+        }
+        catch (const std::exception &e) {
+            std::cerr << e.what() << endl;
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+    }
+
+    for (int i = 0; i < paz_sk; i++) {
+        pazymys = rand() % 10 + 1;
+        paz_.push_back(pazymys);
+        sum += pazymys;
+        cout << i + 1 << " pažymys: " << paz_.at(i) << endl;
+    }
+
+    egz_ = rand() % 10 + 1;
+    cout << "Egzamino įvertinimas: " << egz_ << endl;
+
+    paskaiciuoti_vid_ir_med();
+    paskaiciuoti_gal();
+}
