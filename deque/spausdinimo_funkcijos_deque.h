6b3191c38df7eb8f97f2520eb50213f49664a780
Valentinas <181562313+Valentukis@users.noreply.github.com>
2025-04-13 13:30:21 +0300

    Istrinti nereikalingus failus

--- deque/spausdinimo_funkcijos_deque.h
+++
@@ -1,8 +0,0 @@
-#ifndef spausdinimo_H
-#define spausdinimo_H
-
-#include "mano_lib_deque.h"
-void spausdinimas_kartu(deque<Stud> &grupe, bool spausdinimas);
-void spausdinimas_atskiras(deque<Stud> &grupe);
-int eigos_parinktis();
-#endif
--- list/spausdinimo_funkcijos_list.h
+++
@@ -1,8 +0,0 @@
-#ifndef spausdinimo_H
-#define spausdinimo_H
-
-#include "mano_lib_list.h"
-void spausdinimas_kartu(list<Stud> &grupe, bool spausdinimas);
-void spausdinimas_atskiras(list<Stud> &grupe);
-int eigos_parinktis();
-#endif
